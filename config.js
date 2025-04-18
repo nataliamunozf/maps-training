84281726c93456f23a9b60cb37fdd0fb7211285c
nataliamunozf <168356442+nataliamunozf@users.noreply.github.com>
2025-04-18 19:59:22 +0200

    Faros La Palma desde plantilla

---
+++ config.js
@@ -0,0 +1,117 @@
+var config = {
+    style: 'mapbox://styles/nataliamf/cm9mytucq007w01sba0968uvv',
+    // leave commented to use Mapbox Standard Style
+    accessToken: 'pk.eyJ1IjoibmF0YWxpYW1mIiwiYSI6ImNtOTFteDd5aDAyYjkyaXI0aWJ6M2I5anUifQ.ixzpv39s0a-gNWierYxC2A',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    insetOptions: {
+        markerColor: 'orange'
+    },
+    insetPosition: 'bottom-right',
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    auto: false,
+    title: 'Faros de La Palma',
+    subtitle: 'Tutorial de Storymaps',
+    byline: 'por Natalia Muñoz',
+    footer: 'Fuente: <a href="https://datos.gob.es/" target="_blank">https://datos.gob.es/</a><br> Creado usando <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Tazacorte',
+            image: './assets/san-fran.jpeg',
+            description: 'The first chapter contains a title, image, and camera view for San Francisco, California. Update the chapter data to make it your own.',
+            location: {
+                center: [-17.925664774023392, 28.596661090061634],
+                zoom: 15,
+                pitch: 60,
+                bearing: 70,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Fuencaliente',
+            image: './assets/washington-dc.jpg',
+            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
+            location: {
+                center: [-17.843105021249283, 28.455280989755224],
+                zoom: 15,
+                pitch: 60,
+                bearing: 70,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Villa de Mazo',
+            image: './assets/geneva.jpg',
+            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains. Known as a global hub for diplomacy and finance, it is home to numerous international organizations, including the United Nations and the Red Cross.',
+            location: {
+                center: [-17.7604976022621, 28.56991387610224],
+                zoom: 15,
+                pitch: 60,
+                bearing: 70,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Barlovento',
+            image: './assets/buenos-aires.jpg',
+            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            location: {
+                center: [-17.77804716266497, 28.83902827105873],
+                zoom: 15,
+                pitch: 60,
+                bearing: 70,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
